d2c571fe9f89f3b5386c02b3810264bf990c4d7d
Yu Zhanbei <awesome.zhanbei@gmail.com>
2019-11-08 21:11:52 +0800

    [vhss/main] Stashing current entrance as vhss(virtual-host-static-server).
    - Starting a new entrance to investigate mongodb drivers to record logs.

---
+++ vhss/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+	. "github.com/zhanbei/static-server/libs"
+)
+
+var ops = new(ServerOptions)
+
+func init() {
+	ops.TrustProxyIp = true
+}
+
+// The primary program entrance.
+// Support more custom built, like for lite/medium/heavy programs, for cli/gui(with different themes) modes, and for linux/windows/mac platforms.
+// @see [Support multiple entrances and keep the current one as the primary. · Issue #6 · zhanbei/static-server](https://github.com/zhanbei/static-server/issues/6)
+func main() {
+	app := cli.NewApp()
+	app.Name = "static-server"
+	app.Usage = "A static server in Go, supporting hosting static files in the no-trailing-slash version."
+	app.Version = "0.9.0"
+	app.Description = "A static server in Go, supporting hosting static files in the no-trailing-slash version."
+	app.UsageText = "static-server [global options] [<http-address>:]<http-port> <www-root-directory>"
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name: OptionNameEnableVirtualHosting,
+
+			Usage: "Whether to enable virtual hosting; @see https://en.wikipedia.org/wiki/Virtual_hosting",
+
+			Destination: &ops.UsingVirtualHost,
+		},
+		cli.BoolFlag{
+			Name: OptionNameNoTrailingSlash,
+
+			Usage: "Hosting static files in the " + OptionNameNoTrailingSlash + " mode.",
+
+			Destination: &ops.NoTrailingSlash,
+		},
+		cli.BoolFlag{
+			Name: OptionNameDirectoryListing,
+
+			Usage: "Listing files of a directory if the index.html is not found when in the normal mode.",
+
+			Destination: &ops.DirectoryListing,
+		},
+	}
+	app.Action = Action
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Action(c *cli.Context) error {
+	ops.ValidateOrExit()
+
+	if c.NArg() <= 0 {
+		log.Fatal("Please specify a port, like `static-server 8080`.")
+	}
+	address := c.Args().Get(0)
+	address, _ = ValidateArgAddressOrExit(address)
+
+	rootDir := "."
+	if c.NArg() > 1 {
+		rootDir = c.Args().Get(1)
+	}
+	rootDir = ValidateArgRootDirOrExit(rootDir)
+
+	//fmt.Println("listening:", address, mUsingVirtualHost, mNoTrailingSlash)
+	return RealServer(ops, address, rootDir)
+}
