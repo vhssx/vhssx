2788e10dd1661804d916f91a7654e9da6bcaf91d
Yu Zhanbei <awesome.zhanbei@gmail.com>
2019-11-29 21:02:49 +0800

    [db/**_test] Know more about dynamical memories(heap and stacks things).

--- db/recorder_test.go
+++ db/recorder_test.go
@@ -21,6 +21,14 @@ func Test_WRONG_Slices(t *testing.T) {
 	fmt.Println("a, b:", a, b)
 }
 
+func Test_WRONG_Number(t *testing.T) {
+	a := 5
+	b := &a
+	fmt.Println("a, b, *b:", a, b, *b)
+	a = 32
+	fmt.Println("a, b, *b:", a, b, *b)
+}
+
 func Test_RIGHT_Slices(t *testing.T) {
 	var a [5]int
 	var b *[5]int
